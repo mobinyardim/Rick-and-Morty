9a4347ddb3fd5fb8362ff00f1106facdf8f0d97e
mobin yardim <mobinyardim@gmail.com>
2022-03-09 20:48:22 +0330

    create character model class

---
+++ libraries/core/models/src/main/java/ir/mobinyardim/app/models/Character.kt
@@ -0,0 +1,38 @@
+package ir.mobinyardim.app.models
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val episode: List<String>,
+    val gender: Gender,
+    val image: String,
+    val location: Location,
+    val origin: Origin,
+    val species: String,
+    val status: Status,
+    val type: String,
+    val url: String
+) {
+    enum class Gender {
+        FEMALE,
+        MALE,
+        GENDERLESS,
+        UNKNOWN
+    }
+
+    enum class Status {
+        ALIVE,
+        DEAD,
+        UNKNOWN
+    }
+
+    data class Location(
+        val name: String,
+        val url: String
+    )
+
+    data class Origin(
+        val name: String,
+        val url: String
+    )
+}
