b0b6213fcca4e0b7ba23d3c4d9b0b44f8cf9c6ae
mobin yardim <mobinyardim@gmail.com>
2022-03-15 18:33:18 +0330

    add dao annotation to character dao

--- libraries/core/database/src/main/java/ir/mobinyardi/app/database/daos/CharacterDao.kt
+++ libraries/core/database/src/main/java/ir/mobinyardi/app/database/daos/CharacterDao.kt
@@ -1,9 +1,11 @@
 package ir.mobinyardi.app.database.daos
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import ir.mobinyardi.app.database.models.CharacterEntity
 
+@Dao
 interface CharacterDao {
 
     @Insert
