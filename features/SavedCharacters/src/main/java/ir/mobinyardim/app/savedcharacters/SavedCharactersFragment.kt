0f67dc9002ff7d3b3a34073862fba9828caddab0
mobin yardim <mobinyardim@gmail.com>
2022-03-15 19:06:46 +0330

    add new instance static function

--- features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/SavedCharactersFragment.kt
+++ features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/SavedCharactersFragment.kt
@@ -1,8 +1,18 @@
 package ir.mobinyardim.app.savedcharacters
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 
 class SavedCharactersFragment : Fragment(R.layout.fragment_saved_chracters) {
 
+
+    companion object {
+        fun newInstance(): SavedCharactersFragment {
+            val args = Bundle()
+            return SavedCharactersFragment().apply {
+                arguments = args
+            }
+        }
+    }
 }
