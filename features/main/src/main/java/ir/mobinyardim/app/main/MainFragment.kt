e6c7c9bc34c0cff83abdedf7161bfa91871238d0
mobin yardim <mobinyardim@gmail.com>
2022-03-15 19:44:37 +0330

    initial view pager and tabbar

--- features/main/src/main/java/ir/mobinyardim/app/main/MainFragment.kt
+++ features/main/src/main/java/ir/mobinyardim/app/main/MainFragment.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import ir.mobinyardim.app.main.databinding.FragmentMainBinding
+import ir.mobinyardim.app.screens.ScreenProvidersStore
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment(
+    private val screenProvidersStore: ScreenProvidersStore
+) : Fragment(R.layout.fragment_main) {
+
 
     private lateinit var binding: FragmentMainBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
+
+        binding.viewpager.adapter = ViewPagerAdapter(
+            screenProvidersStore = screenProvidersStore,
+            fragment = this
+        )
+
+        TabLayoutMediator(binding.tabBar, binding.viewpager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Characters"
+                }
+                1 -> {
+                    tab.text = "Saved Characters"
+                }
+            }
+
+        }.attach()
     }
 }
