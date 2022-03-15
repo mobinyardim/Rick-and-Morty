ddf83bdaa938552a06a9cda66ea49ab1903d1bab
mobin yardim <mobinyardim@gmail.com>
2022-03-15 19:36:28 +0330

    create new custom fragment factory

---
+++ app/src/main/java/ir/mobinyardim/rickandmorty/di/MyFragmentFactory.kt
@@ -0,0 +1,28 @@
+package ir.mobinyardim.rickandmorty.di
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import ir.mobinyardim.app.characterdetail.CharacterDetailFragment
+import ir.mobinyardim.app.main.MainFragment
+import ir.mobinyardim.app.screens.ScreenProvidersStore
+import javax.inject.Inject
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class MyFragmentFactory @Inject constructor(
+    private val screenProvidersStore: ScreenProvidersStore
+) : FragmentFactory() {
+
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when (className) {
+            MainFragment::class.java.name -> {
+                MainFragment(screenProvidersStore = screenProvidersStore)
+            }
+            CharacterDetailFragment::class.java.name -> {
+                CharacterDetailFragment()
+            }
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
