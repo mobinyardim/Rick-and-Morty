db602bc264ac74dfcbb8081f720d8bee3ab7db06
mobin yardim <mobinyardim@gmail.com>
2022-03-15 19:11:30 +0330

    Add new instance function

--- features/characters/src/main/java/ir/mobinyardim/app/characters/CharactersFragment.kt
+++ features/characters/src/main/java/ir/mobinyardim/app/characters/CharactersFragment.kt
@@ -1,8 +1,17 @@
 package ir.mobinyardim.app.characters
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 
 class CharactersFragment : Fragment(R.layout.fragment_characters) {
 
+    companion object{
+        fun newInstance(): CharactersFragment{
+            val args = Bundle()
+            return CharactersFragment().apply {
+                arguments=args
+            }
+        }
+    }
 }
