67ab72afaa84849c0e0c7bbb482a3c3e30eace5b
mobin yardim <mobinyardim@gmail.com>
2022-03-15 19:07:46 +0330

    implement saved characters fragment provider

---
+++ features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/di/SavedCharactersModule.kt
@@ -0,0 +1,26 @@
+package ir.mobinyardim.app.savedcharacters.di
+
+import androidx.fragment.app.Fragment
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ir.mobinyardim.app.savedcharacters.SavedCharactersFragment
+import ir.mobinyardim.app.screens.SavedCharactersFragmentProvider
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SavedCharactersModule {
+
+    @Provides
+    @Singleton
+    fun provideSavedCharactersProvider(): SavedCharactersFragmentProvider {
+        return object : SavedCharactersFragmentProvider {
+            override fun newInstance(): Fragment {
+                return SavedCharactersFragment.newInstance()
+            }
+
+        }
+    }
+}
