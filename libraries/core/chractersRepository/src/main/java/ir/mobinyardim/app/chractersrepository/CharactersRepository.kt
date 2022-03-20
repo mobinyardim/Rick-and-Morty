3381b24fe2201ba69d6810a0353aa719328a80f1
mobin yardim <mobinyardim@gmail.com>
2022-03-21 00:23:19 +0330

    change get all characters function return type to flow

--- libraries/core/chractersRepository/src/main/java/ir/mobinyardim/app/chractersrepository/CharactersRepository.kt
+++ libraries/core/chractersRepository/src/main/java/ir/mobinyardim/app/chractersrepository/CharactersRepository.kt
@@ -2,10 +2,11 @@ package ir.mobinyardim.app.chractersrepository
 
 import androidx.paging.PagingData
 import ir.mobinyardim.app.models.Character
+import kotlinx.coroutines.flow.Flow
 
 interface CharactersRepository {
 
-    fun getAllCharacters(): PagingData<Character>
+    fun getAllCharacters(): Flow<PagingData<Character>>
 
     suspend fun getCharacter(id: Int): Character
 
