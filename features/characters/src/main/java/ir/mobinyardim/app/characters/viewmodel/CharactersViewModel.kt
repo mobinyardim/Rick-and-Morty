13bce5873b9cda5f130dc58a0107efa40bdfbea7
MobinYardim <mobinyardim@gmail.com>
2022-12-15 20:52:18 +0330

    add some enw functions to save and unsave character

--- features/characters/src/main/java/ir/mobinyardim/app/characters/viewmodel/CharactersViewModel.kt
+++ features/characters/src/main/java/ir/mobinyardim/app/characters/viewmodel/CharactersViewModel.kt
@@ -6,11 +6,22 @@ import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.mobinyardim.app.chractersrepository.CharactersRepository
 import javax.inject.Inject
+import ir.mobinyardim.app.models.Character
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    charactersRepository: CharactersRepository
+    private val charactersRepository: CharactersRepository
 ) : ViewModel() {
 
     val characters = charactersRepository.getAllCharacters().cachedIn(viewModelScope)
+
+    fun saveCharacter(character: Character) = viewModelScope.launch(Dispatchers.IO) {
+        charactersRepository.saveCharacter(character)
+    }
+
+    fun unSaveCharacter(character: Character) = viewModelScope.launch(Dispatchers.IO) {
+        charactersRepository.unSaveCharacter(character)
+    }
 }
