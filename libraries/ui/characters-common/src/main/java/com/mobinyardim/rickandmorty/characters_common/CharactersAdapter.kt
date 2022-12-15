00c2206a0e984142b3111a35038bd88a57af8503
MobinYardim <mobinyardim@gmail.com>
2022-12-15 20:38:30 +0330

    add save and un save option to adapter

--- libraries/ui/characters-common/src/main/java/com/mobinyardim/rickandmorty/characters_common/CharactersAdapter.kt
+++ libraries/ui/characters-common/src/main/java/com/mobinyardim/rickandmorty/characters_common/CharactersAdapter.kt
@@ -14,12 +14,17 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ir.mobinyardim.app.models.Character
 
 class CharactersAdapter(
-
+    private val onItemClicked: (Character) -> Unit = {},
+    private val onSaveButtonClicked: (Character) -> Unit = {}
 ) : PagingDataAdapter<Character, CharactersAdapter.CharacterViewHolder>(CharacterDiffUtil) {
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
         val character = getItem(position)
-        holder.bindTo(character)
+        holder.bindTo(
+            character = character,
+            onItemClicked = onItemClicked,
+            onSaveButtonClicked = onSaveButtonClicked
+        )
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
@@ -39,17 +44,36 @@ class CharactersAdapter(
             }
         }
 
-        fun bindTo(character: Character?) {
+        fun bindTo(
+            character: Character?,
+            onItemClicked: (Character) -> Unit,
+            onSaveButtonClicked: (Character) -> Unit
+        ) {
             val image = itemView.findViewById<ImageView>(R.id.image)
+            val saveIcon = itemView.findViewById<ImageView>(R.id.save_icon)
             val name = itemView.findViewById<TextView>(R.id.name)
             val gender = itemView.findViewById<TextView>(R.id.gender)
             if (character != null) {
+                itemView.setOnClickListener {
+                    onItemClicked.invoke(character)
+                }
                 name.text = character.name
                 gender.text = character.gender.name
 
                 Glide.with(itemView).load(character.image)
                     .transform(CenterCrop(), RoundedCorners(25))
                     .into(image)
+
+                val iconId = if (character.isSaved) {
+                    R.drawable.ic_saved
+
+                } else {
+                    R.drawable.ic_not_saved
+                }
+                saveIcon.setImageResource(iconId)
+                saveIcon.setOnClickListener {
+                    onSaveButtonClicked.invoke(character)
+                }
             }
         }
     }
