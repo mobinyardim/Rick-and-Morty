f30386eae84983fce32221bfc48f5f1bbaa1b46b
MobinYardim <mobinyardim@gmail.com>
2022-12-15 20:50:45 +0330

    add some new dao functions

--- libraries/core/database/src/main/java/ir/mobinyardi/app/database/daos/CharacterDao.kt
+++ libraries/core/database/src/main/java/ir/mobinyardi/app/database/daos/CharacterDao.kt
@@ -3,15 +3,19 @@ package ir.mobinyardi.app.database.daos
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import ir.mobinyardi.app.database.models.CharacterEntity
 
 @Dao
 interface CharacterDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(characterEntity: CharacterEntity)
 
     @Delete
     suspend fun delete(characterEntity: CharacterEntity)
 
+    @Query("SELECT * FROM characters")
+    suspend fun getAll(): List<CharacterEntity>
 }
