b1aee83506a4eb8e021e16a3641a6d1954d5dd1e
MobinYardim <mobinyardim@gmail.com>
2022-12-15 19:51:57 +0330

    create VerticalSpaceItemDecoration

---
+++ libraries/ui/characters-common/src/main/java/com/mobinyardim/rickandmorty/characters_common/VerticalSpaceItemDecoration.kt
@@ -0,0 +1,23 @@
+package com.mobinyardim.rickandmorty.characters_common
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class VerticalSpaceItemDecoration(
+    private val verticalSpaceHeight: Int
+) : RecyclerView.ItemDecoration() {
+
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        if (parent.getChildAdapterPosition(view) != (parent.adapter?.itemCount ?: 0)- 1) {
+            outRect.bottom = verticalSpaceHeight
+        }
+    }
+
+}
