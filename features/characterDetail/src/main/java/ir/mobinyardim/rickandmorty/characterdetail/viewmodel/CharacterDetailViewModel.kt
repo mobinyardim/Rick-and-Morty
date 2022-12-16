6287e099b07e1062e3c1540398626acf1fa6842b
MobinYardim <mobinyardim@gmail.com>
2022-12-17 01:06:40 +0330

    add factory for viewmodel

--- features/characterDetail/src/main/java/ir/mobinyardim/rickandmorty/characterdetail/viewmodel/CharacterDetailViewModel.kt
+++ features/characterDetail/src/main/java/ir/mobinyardim/rickandmorty/characterdetail/viewmodel/CharacterDetailViewModel.kt
@@ -1,8 +1,10 @@
 package ir.mobinyardim.rickandmorty.characterdetail.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import ir.mobinyardim.rickandmorty.chractersrepository.CharactersRepository
 import ir.mobinyardim.rickandmorty.models.Character
@@ -14,6 +16,24 @@ class CharacterDetailViewModel @AssistedInject constructor(
     @Assisted val characterId: Int,
     private val charactersRepository: CharactersRepository
 ) : ViewModel() {
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: Factory,
+            characterId: Int
+        ): ViewModelProvider.Factory =
+            object : ViewModelProvider.NewInstanceFactory() {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return assistedFactory.create(characterId) as T
+                }
+            }
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(categoryId: Int): CharacterDetailViewModel
+    }
 
     val character = charactersRepository.getCharacter(characterId)
 
