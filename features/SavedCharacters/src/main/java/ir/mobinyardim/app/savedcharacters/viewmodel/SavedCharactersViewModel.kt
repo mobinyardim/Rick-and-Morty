b7583743c51b8e71a137ca3797d0da3db0317488
MobinYardim <mobinyardim@gmail.com>
2022-12-16 21:34:53 +0330

    create new viewmodel class for saved characters

---
+++ features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/viewmodel/SavedCharactersViewModel.kt
@@ -0,0 +1,23 @@
+package ir.mobinyardim.app.savedcharacters.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.mobinyardim.app.chractersrepository.CharactersRepository
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ir.mobinyardim.app.models.Character
+
+@HiltViewModel
+class SavedCharactersViewModel @Inject constructor(
+    private val charactersRepository: CharactersRepository
+) : ViewModel() {
+
+    val savedCharacters = charactersRepository.getSavedCharactersAsPaging().cachedIn(viewModelScope)
+
+    fun unSaveCharacter(character: Character) = viewModelScope.launch(Dispatchers.IO) {
+        charactersRepository.unSaveCharacter(character)
+    }
+}
