452da257ae60c1ad37de9ad41cd0fc4dcb78c47e
MobinYardim <mobinyardim@gmail.com>
2022-12-16 20:45:16 +0330

    map characters with saved characters

--- features/characters/src/main/java/ir/mobinyardim/app/characters/viewmodel/CharactersViewModel.kt
+++ features/characters/src/main/java/ir/mobinyardim/app/characters/viewmodel/CharactersViewModel.kt
@@ -3,11 +3,13 @@ package ir.mobinyardim.app.characters.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import androidx.paging.map
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.mobinyardim.app.chractersrepository.CharactersRepository
 import javax.inject.Inject
 import ir.mobinyardim.app.models.Character
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -15,7 +17,16 @@ class CharactersViewModel @Inject constructor(
     private val charactersRepository: CharactersRepository
 ) : ViewModel() {
 
-    val characters = charactersRepository.getAllCharacters().cachedIn(viewModelScope)
+    private val savedCharacters = charactersRepository.getSavedCharacters()
+
+    val characters = combine(
+        savedCharacters,
+        charactersRepository.getAllCharacters().cachedIn(viewModelScope)
+    ) { savedCharacters, pagedCharacters ->
+        pagedCharacters.map {
+            it.copy(isSaved = savedCharacters.contains(it))
+        }
+    }
 
     fun saveCharacter(character: Character) = viewModelScope.launch(Dispatchers.IO) {
         charactersRepository.saveCharacter(character)
