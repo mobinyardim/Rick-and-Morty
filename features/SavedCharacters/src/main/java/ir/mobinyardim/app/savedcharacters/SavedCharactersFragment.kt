d4bb25991e9639659949c3798a9cbd5f89625fb2
MobinYardim <mobinyardim@gmail.com>
2022-12-16 21:35:50 +0330

    intilize recycler view and show characters

--- features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/SavedCharactersFragment.kt
+++ features/SavedCharacters/src/main/java/ir/mobinyardim/app/savedcharacters/SavedCharactersFragment.kt
@@ -1,9 +1,21 @@
 package ir.mobinyardim.app.savedcharacters
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.RecyclerView
+import com.mobinyardim.rickandmorty.characters_common.CharactersAdapter
+import com.mobinyardim.rickandmorty.characters_common.VerticalSpaceItemDecoration
+import dagger.hilt.android.AndroidEntryPoint
+import ir.mobinyardim.app.savedcharacters.viewmodel.SavedCharactersViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 
+@AndroidEntryPoint
 class SavedCharactersFragment : Fragment(R.layout.fragment_saved_chracters) {
 
 
@@ -12,6 +24,37 @@ class SavedCharactersFragment : Fragment(R.layout.fragment_saved_chracters) {
             val args = Bundle()
             return SavedCharactersFragment().apply {
                 arguments = args
+            }
+        }
+    }
+
+    private val viewModel by viewModels<SavedCharactersViewModel>()
+    private val adapter = CharactersAdapter(
+        onItemClicked = {
+
+        },
+        onSaveButtonClicked = {
+            if(it.isSaved){
+                viewModel.unSaveCharacter(it)
+            }
+        }
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recycler = view.findViewById<RecyclerView>(R.id.characters_recycler)
+        recycler.addItemDecoration(
+            VerticalSpaceItemDecoration(
+                resources.getDimension(com.mobinyardim.rickandmorty.characters_common.R.dimen.item_vertical_space)
+                    .toInt()
+            )
+        )
+        recycler.adapter = adapter
+
+        lifecycleScope.launchWhenCreated {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.savedCharacters.collectLatest {
+                    adapter.submitData(it)
+                }
             }
         }
     }
