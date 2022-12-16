096488d3e1aec94da140ddd1687c0e9afc2f590d
MobinYardim <mobinyardim@gmail.com>
2022-12-17 02:25:17 +0330

    inject constructor and change navigate id

--- features/characterDetail/src/main/java/ir/mobinyardim/rickandmorty/characterdetail/di/CharacterDetailRouteContractImpl.kt
+++ features/characterDetail/src/main/java/ir/mobinyardim/rickandmorty/characterdetail/di/CharacterDetailRouteContractImpl.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import ir.mobinyardim.app.characterdetail.R
 import ir.mobinyardim.rickandmorty.route_contracts.CharacterDetailRouteContract
+import javax.inject.Inject
 
-class CharacterDetailRouteContractImpl : CharacterDetailRouteContract {
+
+class CharacterDetailRouteContractImpl @Inject constructor() : CharacterDetailRouteContract {
 
     override fun show(navController: NavController, characterId: Int) {
         navController.navigate(
-            R.id.characterDetailFragment,
+            R.id.nav_graph_character_detail,
             args = Bundle().apply {
                 putInt(CharacterDetailRouteContract.CHARACTER_ID_KEY, characterId)
             }
