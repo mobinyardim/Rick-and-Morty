f8c83034f64206c5fb70cf4e3946b825364c0812
MobinYardim <mobinyardim@gmail.com>
2022-12-17 22:43:23 +0330

    inject field instaed of inject to constructor

--- features/main/src/main/java/ir/mobinyardim/rickandmorty/main/MainFragment.kt
+++ features/main/src/main/java/ir/mobinyardim/rickandmorty/main/MainFragment.kt
@@ -2,29 +2,33 @@ package ir.mobinyardim.rickandmorty.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 import ir.mobinyardim.app.main.R
-import ir.mobinyardim.app.main.databinding.FragmentMainBinding
 import ir.mobinyardim.rickandmorty.screens.ScreenProvidersStore
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment(
-    private val screenProvidersStore: ScreenProvidersStore
+
 ) : Fragment(R.layout.fragment_main) {
 
-
-    private lateinit var binding: FragmentMainBinding
+    @Inject
+    lateinit var  screenProvidersStore: ScreenProvidersStore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = DataBindingUtil.bind(view)!!
+        val viewPager = view.findViewById<ViewPager2>(R.id.viewpager)
+        val tabBar = view.findViewById<TabLayout>(R.id.tab_bar)
 
-        binding.viewpager.adapter = ViewPagerAdapter(
+        viewPager.adapter = ViewPagerAdapter(
             screenProvidersStore = screenProvidersStore,
             fragment = this
         )
 
-        TabLayoutMediator(binding.tabBar, binding.viewpager) { tab, position ->
+        TabLayoutMediator(tabBar, viewPager) { tab, position ->
             when (position) {
                 0 -> {
                     tab.text = "Characters"
