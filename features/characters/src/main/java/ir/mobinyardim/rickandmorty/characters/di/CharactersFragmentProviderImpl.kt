377b3e8115770811f0dc7a4bed7cd176ef180962
MobinYardim <mobinyardim@gmail.com>
2022-12-17 22:43:37 +0330

    inject constructor

--- features/characters/src/main/java/ir/mobinyardim/rickandmorty/characters/di/CharactersFragmentProviderImpl.kt
+++ features/characters/src/main/java/ir/mobinyardim/rickandmorty/characters/di/CharactersFragmentProviderImpl.kt
@@ -3,8 +3,9 @@ package ir.mobinyardim.rickandmorty.characters.di
 import androidx.fragment.app.Fragment
 import ir.mobinyardim.rickandmorty.characters.CharactersFragment
 import ir.mobinyardim.rickandmorty.screens.CharactersFragmentProvider
+import javax.inject.Inject
 
-class CharactersFragmentProviderImpl : CharactersFragmentProvider {
+class CharactersFragmentProviderImpl @Inject constructor() : CharactersFragmentProvider {
     override fun newInstance(): Fragment {
         return CharactersFragment.newInstance()
     }
